a25fef976812480f9d22fac00494aafc1f376b4a
Charif Mews <charif@westaysafe.com>
2023-05-31 23:28:05 +0200

    Go for caching approach instead of direct request to SxT

--- calculate-ad-slot.js
+++ calculate-ad-slot.js
@@ -12,14 +12,15 @@ if (!publisherId) {
 // Max retries HTTP requests are 4 because total HTTP requests a Chainlink Function can do is 5 https://docs.chain.link/chainlink-functions/resources/service-limits
 function httpRequest(url, headers, data, retries = 4) {
   return new Promise((resolve, reject) => {
-    Functions.makeHttpRequest({ url, headers, data })
+    // Timeout 3000ms is because of the chainlink service limits
+    Functions.makeHttpRequest({ url, headers, data, method: "POST" })
       .then((response) => {
         if (response.statusText === "OK") {
           resolve(response.data);
         } else if (retries > 0) {
           console.log(`Retry attempts remaining: ${retries}`);
           setTimeout(() => {
-            httpRequest(url, headers, retries - 1)
+            httpRequest(url, headers, data, retries - 1)
               .then(resolve)
               .catch(reject);
           }, 1000); // retry after 1 second
@@ -31,7 +32,7 @@ function httpRequest(url, headers, data, retries = 4) {
         if (retries > 0) {
           console.log(`Retry attempts remaining: ${retries}`);
           setTimeout(() => {
-            httpRequest(url, headers, retries - 1)
+            httpRequest(url, headers, data, retries - 1)
               .then(resolve)
               .catch(reject);
           }, 1000); // retry after 1 second
@@ -41,27 +42,19 @@ function httpRequest(url, headers, data, retries = 4) {
       });
   });
 }
-// TODO: Test speed SpaceAndTime query speed after inserting a lot of events
-// with Kafka producer and discover where the pain points are
 
-// TODO: SpaceAndTime url for API call
-// There are three approaches, discuss during office hours
-  // 1. Get the total views, minus the reserved ads and write it on chain so it can be 
-  // automatically populated in spacexTime table when that feature is ready, but what to do with amount that is left?
-  // 2. Do a write directly to the SpaceAndTime table that is idempotent, I can probably use the requestID for this
-  // 3. Let only one node do the write to SpaceAndTime table, but this feels like a single point of failure
-httpRequest("https://hackathon.spaceandtime.dev/v1/sql/dql", {
-  'Authorization': `Bearer ${ secrets.accessToken }`,
-    "Content-Type": "application/json"
-}, {
-  "resourceId": "cardclub.events", 
-  "sqlText": sqlText
-})
+const boughtAdViews = httpRequest(
+  "https://card.club/api/ads",
+  {
+    // TODO: Add authorization header later
+    // 'Authorization': `Bearer ${ secrets.accessToken }`,
+    "Content-Type": "application/json",
+  },
+  { publisherId: publisherId, linkAmount: linkAmount }
+)
   .then((data) => {
-    // TODO: SpaceAndTime query
-    console.log(data);
+    return Functions.encodeUint256(data.adViews);
   })
   .catch((error) => console.error(error));
 
-// TODO: Remove hardcoded value for ad slot
-return Functions.encodeUint256(173);
+return boughtAdViews;

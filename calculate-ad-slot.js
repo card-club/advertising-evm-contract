4cbb669e14ed7011110d3d82c979ac93fec63423
Charif Mews <charif@westaysafe.com>
2023-06-02 21:30:12 +0200

    Add logging to see why it works locally, but not in Github action

--- calculate-ad-slot.js
+++ calculate-ad-slot.js
@@ -18,6 +18,7 @@ function httpRequest(url, headers, data, retries = 4) {
         if (response.statusText === "OK") {
           resolve(response.data);
         } else if (retries > 0) {
+          console.log(response);
           console.log(`Retry attempts remaining: ${retries}`);
           setTimeout(() => {
             httpRequest(url, headers, data, retries - 1)
@@ -29,6 +30,7 @@ function httpRequest(url, headers, data, retries = 4) {
         }
       })
       .catch((error) => {
+        console.log(error);
         if (retries > 0) {
           console.log(`Retry attempts remaining: ${retries}`);
           setTimeout(() => {
